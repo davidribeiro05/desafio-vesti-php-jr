e7e1a84771587e7fb1cc3413199a99e48ade2d67
David Ribeiro <david.ribeiro05@gmail.com>
2021-07-18 14:11:46 -0300

    Rotas filtradas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Imagem;
-use App\Http\Controllers\Produto;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Imagem;
+use App\Http\Controllers\Api\Produto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,17 +16,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['apiJwt']], function () {
+    Route::post('/produto', [Produto::class, 'store']);
+    Route::get('/produto/{id}', [Produto::class, 'show']);
+    Route::patch('/produto/{id}', [Produto::class, 'update']);
+    Route::delete('/produto/{id}', [Produto::class, 'destroy']);
+
+    Route::post('/imagem', [Imagem::class, 'store']);
+    Route::get('/imagem/{id}', [Imagem::class, 'show']);
+    Route::patch('/imagem/{id}', [Imagem::class, 'update']);
+    Route::delete('/imagem/{id}', [Imagem::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-Route::post('/produto', [Produto::class, 'store']);
-Route::get('/produto/{id}', [Produto::class, 'show']);
-Route::patch('/produto/{id}', [Produto::class, 'update']);
-Route::delete('/produto/{id}', [Produto::class, 'destroy']);
-
-Route::post('/imagem', [Imagem::class, 'store']);
-Route::get('/imagem/{id}', [Imagem::class, 'show']);
-Route::patch('/imagem/{id}', [Imagem::class, 'update']);
-Route::delete('/imagem/{id}', [Imagem::class, 'destroy']);
